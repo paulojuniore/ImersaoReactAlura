59154ec34fa6b4b12190220daaaff65ebb33508d
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-07-31 18:18:03 -0300

    Added category component and use state to manipulate static d datas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import './Menu.css';
 
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/pauloflix.png';
 
 // Componenete do botão criado com css puro
-//import ButtonLink from '../ButtonLink'
+// import ButtonLink from '../ButtonLink'
 
 // Componente do botão criado com styled-components
 import Button from '../Button';
-
-import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
     <nav className="Menu">
       <Link to="/">
-        <img className="Logo" src={Logo} alt="Pauloflix logo"/>
+        <img className="Logo" src={Logo} alt="Pauloflix logo" />
       </Link>
 
       <Button as={Link} to="/cadastro/video" className="ButtonLink">
         Novo vídeo
       </Button>
     </nav>
-  )
+  );
 }
 
 export default Menu;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,103 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+
+  const [values, setValues] = useState(valoresIniciais);
+  const [categorias, setCategorias] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCategorias([
+      ...categorias,
+      values,
+    ]);
+    setValues(valoresIniciais);
+  };
+
+  const setValue = (key, value) => {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  };
+
+  const handleChange = (e) => {
+    const { getAttribute, value } = e.target;
+    setValue(
+      getAttribute('name'),
+      value,
+    );
+  };
+
   return (
     <PageDefault>
-      <h1>Cadastro de categorias</h1>
+      <h1>Cadastro de categoria</h1>
 
-      <Link to='/cadastro/video'>
-        Novo vídeo
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Nome da categoria:
+            <input
+              type="text"
+              name="nome"
+              value={values.nome}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              name="descricao"
+              value={values.descricao}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              name="cor"
+              value={values.cor}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <button type="submit">
+          Cadastrar
+        </button>
+
+        <ul>
+          { categorias.map((categoria, index) => (
+            <li key={`${categoria}-${index}`}>
+              { categoria.nome }
+            </li>
+          )) }
+        </ul>
+      </form>
+
+      <Link to="/">
+        Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
