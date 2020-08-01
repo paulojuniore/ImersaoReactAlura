a2bf7a9c8abd3070b00bfd3c500c2a0178afb3af
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-08-01 13:10:16 -0300

    Stylization of the FormField component.

--- src/components/ButtonLink/index.js
+++ src/components/ButtonLink/index.js
@@ -1,11 +1,13 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 function ButtonLink(props) {
   return (
-    <a href={ props.href } className={ props.className }>
+    <a href={props.href} className={props.className}>
       { props.children }
     </a>
-  )
+  );
 }
 
 export default ButtonLink;
